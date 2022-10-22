b000d61690dbdd01277f5a4ce3ebbd20b8fadedc
daniel <daniel.dl14121@gmail.com>
2022-10-22 00:50:45 -0600

    tipos de datos, clase 1 terminado

---
+++ src/sesiones_1_2_3/tiposdatos/Main.java
@@ -0,0 +1,26 @@
+package tiposdatos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // enteros
+        byte variable1 = 5;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+        long numeroTelefono = 666555444;
+
+        // decimales
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+
+        // booleano
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        // texto
+        char variable9 = 'a';
+        String variable10 = "Lorem ipsum dolor sit amet.....";
+        System.out.println(variable1);
+    }
+}
