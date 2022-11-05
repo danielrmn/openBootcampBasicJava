a99751e8adc133025a5f6d3566d0fbef6ff5804c
daniel <daniel.dl14121@gmail.com>
2022-11-05 00:54:09 -0600

    temma 3 done.

---
+++ src/EstructurasDeControl/ArrayTextos.java
@@ -0,0 +1,10 @@
+package EstructurasDeControl;
+
+public class ArrayTextos {
+    public static void main(String[] args) {
+        String[] suplementos = {"creatina," + " proteina," + " cafeina," + " trembolona," + " testosterona"};
+        for (int i = 0; i < 1; i++) {
+            System.out.println(suplementos[i]);
+        }
+    }
+}
